2a7b266f3ef1aaeb3b1e32bd5ae3c329ad73d965
Kanishq Sodhani <kanishq.maheshwari2003@gmail.com>
2025-10-05 14:48:11 +0530

    create datasqaure repository

---
+++ README.md
@@ -0,0 +1,3 @@
+# DataSquare
+
+### A platform where people learn and grow in public, swap their skills, show projects and resume and help everyone.
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <div className="w-full h-screen bg-black text-white flex justify-center items-center"></div>
+  );
+};
+
+export default App;
